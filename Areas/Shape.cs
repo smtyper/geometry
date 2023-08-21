6ab1f9a50955eed2d8c969952cbe809c9626b893
Artem <smtyper.contact@gmail.com>
2023-08-22 02:28:36 +0300

    Implement task 1 library

---
+++ Areas/Shape.cs
@@ -0,0 +1,12 @@
+﻿namespace Areas;
+
+/// <summary>
+/// Represents a geometric shape.
+/// </summary>
+public abstract class Shape : IHasArea
+{
+    /// <summary>
+    /// Calculates shape area.
+    /// </summary>
+    public abstract decimal CalculateArea();
+}
