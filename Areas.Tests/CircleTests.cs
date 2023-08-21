f23206c82d970c4b5573d54baf6d10eba1db210e
Artem <smtyper.contact@gmail.com>
2023-08-22 02:28:51 +0300

    Add unit tests

---
+++ Areas.Tests/CircleTests.cs
@@ -0,0 +1,25 @@
+﻿using DecimalMath;
+
+namespace Areas.Tests;
+
+public class CircleTests
+{
+    #region Cases
+
+    [TestCase(1, 3.14)]
+    [TestCase(2, 12.57)]
+    [TestCase(3, 28.27)]
+    [TestCase(4, 50.27)]
+    [TestCase(5, 78.54)]
+    [TestCase(6, 113.1)]
+
+    #endregion
+    [Test]
+    public void CalculateAreaTest(decimal radius, decimal area)
+    {
+        var circle = new Circle(radius);
+        var result = circle.CalculateArea();
+
+        Assert.That(result.RoundFromZero(2), Is.EqualTo(area.RoundFromZero(2)));
+    }
+}
---
+++ Areas.Tests/TriangleTests.cs
@@ -0,0 +1,44 @@
+﻿using DecimalMath;
+
+namespace Areas.Tests;
+
+public class TriangleTests
+{
+    #region Cases
+
+    [TestCase(3, 4, 5, 6)]
+    [TestCase(7, 8, 9, 26.83)]
+    [TestCase(12, 16, 20, 96)]
+    [TestCase(5, 12, 13, 30)]
+    [TestCase(9, 10, 17, 36)]
+
+    #endregion
+    [Test]
+    public void CalculateAreaTest(decimal firstEdge, decimal secondEdge, decimal thirdEdge, decimal area)
+    {
+        var edges = new[] { firstEdge, secondEdge, thirdEdge };
+        var triangle = new Triangle(edges);
+        var result = triangle.CalculateArea();
+
+        Assert.That(result.RoundFromZero(2), Is.EqualTo(area.RoundFromZero(2)));
+    }
+
+    #region Cases
+
+    [TestCase(3, 4, 5, true)]
+    [TestCase(5, 12, 13, true)]
+    [TestCase(6, 8, 10, true)]
+    [TestCase(4, 7, 9, false)]
+    [TestCase(1, 8, 18, false)]
+
+    #endregion
+    [Test]
+    public void IsRightTest(decimal firstEdge, decimal secondEdge, decimal thirdEdge, bool isRight)
+    {
+        var edges = new[] { firstEdge, secondEdge, thirdEdge };
+        var triangle = new Triangle(edges);
+        var result = triangle.IsRight();
+
+        Assert.That(result, Is.EqualTo(isRight));
+    }
+}
